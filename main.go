46ef89f28e4dc97f067379fb339a1db0944981f6
Kai Walter <kai.walter@outlook.de>
2020-02-14 18:03:33 +0100

    loaded ROM

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
-	"github.com/KaiWalter/go6502/mos6502"
+	"log"
+	"os"
 )
+
+// RetrieveROM retrieves contents of a file into memory
+// https://github.com/Klaus2m5/6502_65C02_functional_tests/blob/master/bin_files/6502_functional_test.lst
+func RetrieveROM(filename string) ([]byte, error) {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return nil, fmt.Errorf("error opening file: %v", err)
+	}
+
+	defer file.Close()
+
+	bytes := make([]byte, 0x10000)
+
+	bufr := bufio.NewReader(file)
+
+	_, err = bufr.Read(bytes)
+	if err != nil {
+		return nil, fmt.Errorf("error reading file: %v", err)
+	}
+
+	return bytes, err
+}
 
 func main() {
 	fmt.Println("6502 emulation testing")
-	fmt.Println(mos6502.Test())
+
+	var ram []byte
+	var err error
+
+	ram, err = RetrieveROM("6502_functional_test.bin")
+	if err != nil {
+		log.Fatalf("could not retrieve ROM: %v", err)
+	}
+
+	fmt.Printf("%x\n", ram[0x400])
 }
