f71b95c55beb2d4d6e9ded9ce1a569b2b8cca922
Kai Walter <kai.walter@outlook.de>
2020-02-14 19:07:27 +0100

    starting cpu

--- main.go
+++ main.go
@@ -7,33 +7,34 @@ import (
 	"os"
 )
 
+var ram []byte
+
 // RetrieveROM retrieves contents of a file into memory
 // https://github.com/Klaus2m5/6502_65C02_functional_tests/blob/master/bin_files/6502_functional_test.lst
 func RetrieveROM(filename string) ([]byte, error) {
-	file, err := os.Open(filename)
+	romfile, err := os.Open(filename)
 
 	if err != nil {
 		return nil, fmt.Errorf("error opening file: %v", err)
 	}
 
-	defer file.Close()
+	defer romfile.Close()
 
-	bytes := make([]byte, 0x10000)
+	buffer := make([]byte, 0x10000)
 
-	bufr := bufio.NewReader(file)
+	bufferreader := bufio.NewReader(romfile)
 
-	_, err = bufr.Read(bytes)
+	_, err = bufferreader.Read(buffer)
 	if err != nil {
 		return nil, fmt.Errorf("error reading file: %v", err)
 	}
 
-	return bytes, err
+	return buffer, err
 }
 
 func main() {
 	fmt.Println("6502 emulation testing")
 
-	var ram []byte
 	var err error
 
 	ram, err = RetrieveROM("6502_functional_test.bin")
--- mos6502/main.go
+++ mos6502/main.go
@@ -1,5 +1,32 @@
 package mos6502
 
+// ReadFunc defines a function where the CPU can read from RAM or Bus
+type ReadFunc func(uint16) uint8
+
+// Read points to a function where the CPU can read from RAM or Bus
+var Read ReadFunc
+
+// WriteFunc defines a function where the CPU can write to RAM or Bus
+type WriteFunc func(uint16, uint8)
+
+// Write points to a function where the CPU can write to RAM or Bus
+var Write WriteFunc
+
+// PC = ProgramCounter
+var PC uint16
+
+// SP = Stack Pointer
+var SP uint8
+
+// A Accumulator
+var A uint8
+
+// X Index
+var X uint8
+
+// Y Index
+var Y uint8
+
 // Test just returns a sample
 func Test() string {
 	return "XXX"
