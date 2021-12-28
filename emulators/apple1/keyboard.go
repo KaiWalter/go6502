47d1954fa7f0f6456fa99ba05513cb27d50cf87b
Kai Walter <kai.walter@outlook.de>
2021-12-28 11:35:56 +0100

    added shift and ctrl key mapping

--- emulators/apple1/keyboard.go
+++ emulators/apple1/keyboard.go
@@ -5,44 +5,67 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+type keyMap struct {
+	unmodified byte
+	shifted    byte
+	ctrl       byte
+}
+
 var (
-	keyboardMapping map[sdl.Keycode]byte
+	keyboardMapping map[sdl.Keycode]keyMap
 )
 
 func initKeyboardMapping() {
 
-	keyboardMapping = map[sdl.Keycode]byte{
-		0x08: 0x08,
-		0x0D: 0x0D,
-		0x20: 0x20,
+	// DE!
+	keyboardMapping = map[sdl.Keycode]keyMap{
+		0x08: {unmodified: 0x5F, shifted: 0x5F, ctrl: 0x5F}, // Apple1 keyboard had not backspace - hence _
+		0x0D: {unmodified: 0x0D, shifted: 0x0D, ctrl: 0x0D},
 
-		'*': 0x2A,
-		'/': 0x2B,
-		'+': 0x2F,
-		'-': 0x2D,
+		' ': {unmodified: 0x20, shifted: 0x20, ctrl: 0x20},
 
-		'.': 0x2E,
-		',': 0x2C,
-		// TO DO add missing mappings
+		'.': {unmodified: 0x2E, shifted: 0x3A, ctrl: 0x00},
+		',': {unmodified: 0x2C, shifted: 0x3B, ctrl: 0x00},
+		'+': {unmodified: 0x2B, shifted: 0x2A, ctrl: 0x00},
+		'-': {unmodified: 0x2D, shifted: 0x5F, ctrl: 0x00},
+
+		'0': {unmodified: 0x30, shifted: 0x3D, ctrl: 0x00},
+		'1': {unmodified: 0x31, shifted: 0x21, ctrl: 0x00},
+		'2': {unmodified: 0x32, shifted: 0x22, ctrl: 0x00},
+		'3': {unmodified: 0x33, shifted: 0xA7, ctrl: 0x00},
+		'4': {unmodified: 0x34, shifted: 0x24, ctrl: 0x00},
+		'5': {unmodified: 0x35, shifted: 0x25, ctrl: 0x00},
+		'6': {unmodified: 0x36, shifted: 0x26, ctrl: 0x00},
+		'7': {unmodified: 0x37, shifted: 0x2F, ctrl: 0x00},
+		'8': {unmodified: 0x38, shifted: 0x28, ctrl: 0x00},
+		'9': {unmodified: 0x39, shifted: 0x29, ctrl: 0x00},
 	}
 
-	// map characters A-Z
-	for i := sdl.Keycode(0x61); i <= 0x7a; i++ {
-		keyboardMapping[i] = byte(i - 0x20)
-	}
-	// map digits 0-9
-	for i := sdl.Keycode(0x30); i <= 0x39; i++ {
-		keyboardMapping[i] = byte(i)
+	// map characters @ A-Z
+	for i := sdl.Keycode(0x60); i <= 0x7a; i++ {
+		keyboardMapping[i] = keyMap{unmodified: byte(i - 0x20), shifted: byte(i - 0x20), ctrl: byte(i - 0x60)}
 	}
 
 }
 
 func handleKeypressed(keysym sdl.Keysym) {
 	keyvalue, exists := keyboardMapping[keysym.Sym]
-	if exists && keyvalue < 0x60 {
-		piaCA1Channel <- mc6821.Fall               // bring keyboard strobe to low to force active transition
-		keyboardInputChannelA <- (keyvalue | 0x80) // bit 7 is constantly set (+5V)
-		piaCA1Channel <- mc6821.Rise               // send only pulse
-		piaCA1Channel <- mc6821.Fall               // 20 micro secs are not worth emulating
+	if exists {
+
+		value := keyvalue.unmodified
+		switch keysym.Mod {
+		case 0x01:
+			value = keyvalue.shifted
+		case 0x40:
+			value = keyvalue.ctrl
+		}
+
+		if value > 0x00 && value < 0x60 {
+			piaCA1Channel <- mc6821.Fall            // bring keyboard strobe to low to force active transition
+			keyboardInputChannelA <- (value | 0x80) // bit 7 is constantly set (+5V)
+			piaCA1Channel <- mc6821.Rise            // send only pulse
+			piaCA1Channel <- mc6821.Fall            // 20 micro secs are not worth emulating
+		}
 	}
+
 }
